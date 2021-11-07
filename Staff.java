eaa8633625298f9cfbd858f1fe0dfd3b3e5738d0
mjk9857 <66678904+mjk9857@users.noreply.github.com>
2021-11-07 13:41:29 +0800

    Create Staff.java

---
+++ Staff.java
@@ -0,0 +1,45 @@
+public class Staff extends Order{
+  private String name;
+  private char gender;
+  private int employeeID;
+  private String jobTitle;
+  
+  public Staff(String name, char gender, int employeeID, String jobTitle) {
+        this.name = name;
+        this.gender = gender;
+        this.employeeID = employeeID;
+        this.jobTitle = jobTitle;
+   }
+  
+  public String getName(){
+    return name;
+  }
+  
+  public void setName(String name){
+    this.name=name;
+  }
+  
+  public char getGender(){
+    return gender;
+  }
+  
+  public void setGender(char gender){
+    this.gender=gender;
+  }
+  
+  public int getEmployeeID(){
+    return employeeID;
+  }
+  
+  public void setEmployeeID(int employeeID){
+    this.employeeID=employeeID;
+  }
+  
+  public String getJobTitle(){
+    return jobTitle;
+  }
+  
+  public String setJobTitle(String jobTitle){
+    this.jobTitle=jobTitle;
+  }
+  
